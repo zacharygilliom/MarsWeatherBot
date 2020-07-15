00a58c62785f243deea06028011abcc8449b2b14
Zachary Gilliom <zacharygilliom@gmail.com>
2020-07-14 21:27:10 -0400

    Added functionality to check for empty API call from the nasadata pkg.

--- pkg/twitter/reply.go
+++ pkg/twitter/reply.go
@@ -28,20 +28,33 @@ func ReplyAbout(user string, client *twitter.Client) {
 
 func ReplyDayAndWeather(user string, client *twitter.Client) {
 	nasadata := nasaData.GetData()
-	fmt.Println(nasadata)
-	currentDay := nasaData.GetSolDay()
-	fmt.Println(currentDay)
-	//strDay := strconv.Itoa(currentDay)
-	//currentDayTemp := nasaData.GetDayTemp(currentDay, data)
-	//currentTempStr := fmt.Sprintf("%.2f", currentDayTemp)
-	listday := nasaData.GetListDays(nasadata)
-	fmt.Println(listday)
-	body := ConfigureTweet(listday, currentDay, nasadata)
-	fmt.Println(body)
-	tweetBody := "@" + user + " " + body
-	fmt.Println(tweetBody)
-	//body := "@" + user + "\n Current Mars Sol Day: " + strDay + "\n Current Temperature: " + currentTempStr
-	NewTweet(client, tweetBody)
+	if len(nasadata) == 0 {
+		tweetBody := "@" + user + " I'm sorry, but the weather data has not been updated in the past seven days.  Please check back tomorrow."
+		NewTweet(client, tweetBody)
+	} else {
+		currentDay := nasaData.GetSolDay()
+		listday := nasaData.GetListDays(nasadata)
+		body := ConfigureTweet(listday, currentDay, nasadata)
+		tweetBody := "@" + user + " " + body
+		NewTweet(client, tweetBody)
+	}
+	/*
+		fmt.Println(nasadata)
+		currentDay := nasaData.GetSolDay()
+		fmt.Println(currentDay)
+		//strDay := strconv.Itoa(currentDay)
+		//currentDayTemp := nasaData.GetDayTemp(currentDay, data)
+		//currentTempStr := fmt.Sprintf("%.2f", currentDayTemp)
+		listday := nasaData.GetListDays(nasadata)
+		fmt.Println(listday)
+		body := ConfigureTweet(listday, currentDay, nasadata)
+		fmt.Println(body)
+
+		tweetBody := "@" + user + " " + body
+		fmt.Println(tweetBody)
+		//body := "@" + user + "\n Current Mars Sol Day: " + strDay + "\n Current Temperature: " + currentTempStr
+		NewTweet(client, tweetBody)
+	*/
 }
 
 func ReplyHelp(user string, client *twitter.Client) {

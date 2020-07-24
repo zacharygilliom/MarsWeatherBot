10d829e88499fbc051bfeb4a42dae5b1b2ef1bf7
Zachary Gilliom <zacharygilliom@gmail.com>
2020-07-24 01:08:35 -0400

    Removed binary builds, added info.log and logs/ directory.

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/robfig/cron/v3"
-	// "github.com/zacharygilliom/MarsWeatherBot/pkg/client"
 	"github.com/zacharygilliom/MarsWeatherBot/pkg/twitter"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -15,15 +15,20 @@ func main() {
 	/*Create our cron job and execute our daily tweet in the cron job.
 	Also will initialize the stream of tweets
 	*/
+	file, err := os.OpenFile("~/goProjects/MarsWeatherBot/logs/info.log", os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	defer file.Close()
+
+	log.SetOutput(file)
 	c := cron.New()
 	c.AddFunc("30 20 * * *", twitter.PostTweet)
 	c.Start()
-	fmt.Println("CronJob Executed")
 
 	demux, client := twitter.GetMessages()
 	fmt.Println("Stream Started...")
-	// client := client.NewOauth1()
 	stream := twitter.Stream(client)
 	go demux.HandleChan(stream.Messages)
 	ch := make(chan os.Signal)
